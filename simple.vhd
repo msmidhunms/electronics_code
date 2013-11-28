7b05be0adb7025805b928cf08a928a379fce71d0
Midhun.M.S <msmidhunms@gmail.com>
2013-11-28 20:33:15 +0530

    Update simple.vhd

--- simple.vhd
+++ simple.vhd
@@ -1,2 +1,10 @@
 library IEEE;
 use IEEE.std_logic.all;
+entity simple is
+      port(a,b : in std_logic; c: out std_logic);
+end entity simple
+
+architecture behaviour of simple is
+begin
+
+end behaviour
